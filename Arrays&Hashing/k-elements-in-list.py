65d818d83ce3caee8c60d236f9e1375866ee7f7a
Serigne Ousmane Kana Ciss <ousmanekanaciss@gmail.com>
2025-04-07 12:29:55 -0500

    Update k-elements-in-list.py

--- Arrays&Hashing/k-elements-in-list.py
+++ Arrays&Hashing/k-elements-in-list.py
@@ -1,13 +1,9 @@
 from collections import Counter 
-nums = [1,2]
+nums = [1,2,2,3,3,3]
 k = 2
 res = []
 
 nums_hash = Counter(nums)
-
-for key, value in nums_hash.items():
-    if value - k >= 0:
-        res.append(key)
     
 print(nums_hash)
-print(res, list(set(res)))
+print(list(nums_hash.keys()))
